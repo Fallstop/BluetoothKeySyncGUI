47f41e2ee0bdbe4ac500eb88401b8548a58b9109
Jasper Miller-Waugh <contact@jmw.nz>
2025-06-30 16:15:35 +1200

    correct height calculations

--- src/lib/components/bluetooth/syncing/ControllerGroupNode.svelte
+++ src/lib/components/bluetooth/syncing/ControllerGroupNode.svelte
@@ -2,7 +2,7 @@
 	import type { BluetoothDevice } from '#root/bindings';
 	import { Handle, type Node, type NodeProps } from '@xyflow/svelte';
 
-  export type ControllerNodeType = Node<{ controller: BluetoothController, device_length: number }, 'device'>;
+  export type ControllerNodeType = Node<{ controller: BluetoothController, device_height: number }, 'device'>;
 
 	const generalMargin = 1.25;
 	const headerHeight = 2 + generalMargin *2;
@@ -27,11 +27,12 @@
 
 </script>
 
-<div class="border rounded-lg -z-10" style="width: {controllerGroupDimension.width}rem">
+<div class="border rounded-lg -z-10 cursor-default" style="width: {controllerGroupDimension.width}rem">
 	<div
 		class="flex items-center gap-2 w-full text-left rounded p-5 -m-2"
 	>
 		<Bluetooth class="h-4 w-4" />
+		<span class="custom-drag-handle hidden"></span>
 		<div>
 			<div class="font-medium">
 				{data.controller.name || "Bluetooth Controller"}
@@ -43,9 +44,9 @@
 	</div>
 
 	<div
-		class="m-4 space-y-4"
+		class="mx-4 space-y-4"
 		transition:slide={{ axis: "y", duration: 200 }}
 	>
-	<div style="height: {data.device_length * deviceDimension.height}rem;"></div>
+	<div style="height: {data.device_height}rem;"></div>
 	</div>
 </div>
--- src/lib/components/bluetooth/syncing/DeviceCanvas.svelte
+++ src/lib/components/bluetooth/syncing/DeviceCanvas.svelte
@@ -16,24 +16,30 @@
 
 	function deviceColumn(controller: BluetoothController | null, x_initial: number, y_initial: number) {
 		if (!controller) {
-			return [];
+			return {
+				nodes: [],
+				total_height: 0
+			};
 		}
 
 		let devices = (controller?.devices || []);
 
+		let device_height = (devices.length * (deviceDimension.height + controllerGroupDimension.generalMargin));
+
 		let header_node = {
-			id: `${x_initial}-header`,
+			id: `controller-${controller.address}-${x_initial}`,
 			type: "controllerNode",
-			data: { controller, device_length: devices.length },
+			dragHandle: ".custom-drag-handle",
+			data: { controller, device_height: device_height },
 			position: { x: remToPixels(x_initial), y: remToPixels(y_initial) },
 		};
 
 		const headerOffset = controllerGroupDimension.headerHeight;
 
-		return [
+		const nodes = [
 			header_node,
 			... devices.map((device, index) => ({
-				id: `${x_initial}-${index}`,
+				id: `device-${device.address}-${x_initial}-${index}`,
 				type: "deviceNode",
 				data: { device },
 				dragHandle: ".custom-drag-handle",
@@ -41,18 +47,35 @@
 				parentNode: header_node.id,
 			}))
 		];
+
+		const total_height = headerOffset + device_height;
+
+		return {
+			nodes,
+			total_height
+		}
 	}
 
 	function deriveNodes(controllers: MatchedControllers) {
+		const interRowSpacing = 2;
+
+		let currentY = 0;
+
 		return controllers.flatMap((controller, index) => {
-			const y_initial = 0;
+			const y_initial = currentY;
 
 			const interColumnSpacing = 4 + controllerGroupDimension.width;
 
+			const windows = deviceColumn(controller.windows, 0, y_initial);
+			const linux = deviceColumn(controller.linux, interColumnSpacing, y_initial);
+
+			currentY += Math.max(windows.total_height, linux.total_height) + interRowSpacing;
+
 			return [
-				...deviceColumn(controller.windows, 0, y_initial),
-				...deviceColumn(controller.linux, interColumnSpacing, y_initial),
+				...windows.nodes,
+				...linux.nodes,
 			];
+
 		});
 	}
 
@@ -74,12 +97,12 @@
 
 </script>
 
-<div style:width="100vw" style:height="50vh">
+<div style:width="100vw" style:height="100vh">
+	<!-- maxZoom={1}
+	minZoom={1}
+	panOnDrag={false}
+	selectionOnDrag={false} -->
 	<SvelteFlowProvider>
-		<!-- maxZoom={1}
-		minZoom={1}
-		panOnDrag={false}
-		selectionOnDrag={false} -->
 		<SvelteFlow
 		bind:nodes
 		bind:edges
